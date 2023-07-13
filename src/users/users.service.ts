f362ef5ea613a6ad79e84d4c983f37b06753fa5e
duyen.dang2 <duyen.dang2@sotatek.com>
2023-07-13 16:47:44 +0700

    Complete user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  ForbiddenException,
   Inject,
   Injectable,
   UnauthorizedException,
@@ -14,6 +15,7 @@ import { userTypes } from 'src/schema/users';
 import config from 'config';
 import { sendEmail } from 'src/utility/mail-handler';
 import { generateAuthToken } from 'src/utility/token-generator';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -120,6 +122,197 @@ export class UsersService {
             type,
             id: _id.toString(),
           },
+          token,
+        },
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async verifyEmail(otp: string, email: string) {
+    try {
+      const user = await this.userDB.findOne({ email });
+
+      if (!user) {
+        throw new BadRequestException('User not found');
+      }
+
+      if (user.otp !== otp) {
+        throw new BadRequestException('Invalid Otp');
+      }
+
+      if (user.otpExpiryTime < new Date()) {
+        throw new BadRequestException('Otp expired');
+      }
+
+      await this.userDB.updateOne(
+        {
+          email,
+        },
+        {
+          isVerified: true,
+        },
+      );
+
+      return {
+        success: true,
+        message: 'Email verified successfully. You can login now',
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async sendOtpEmail(email: string) {
+    try {
+      const user = await this.userDB.findOne({ email });
+
+      if (!user) {
+        throw new BadRequestException('User not found');
+      }
+
+      if (user.isVerified) {
+        throw new BadRequestException('Email already verified');
+      }
+      const otp = Math.floor(Math.random() * 900000) + 100000;
+
+      const otpExpiryTime = new Date();
+      otpExpiryTime.setMinutes(otpExpiryTime.getMinutes() + 10);
+
+      await this.userDB.updateOne(
+        {
+          email,
+        },
+        { otp, otpExpiryTime },
+      );
+
+      sendEmail(
+        user.email,
+        config.get('emailService.emailTemplates.verifyEmail'),
+        'Email verification - HuyHung',
+        {
+          customName: user.name,
+          customEmail: user.email,
+          otp,
+        },
+      );
+
+      return {
+        success: true,
+        message: 'OTP sent successfully',
+        result: { email: user.email },
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async forgotPassword(email: string) {
+    try {
+      const user = await this.userDB.findOne({ email });
+
+      if (!user) {
+        throw new BadRequestException('User not found');
+      }
+      let password = Math.random().toString(36).substring(2, 12);
+      const tempPassword = password;
+      password = await generateHashPassword(password);
+      await this.userDB.updateOne(
+        {
+          _id: user._id,
+        },
+        { password },
+      );
+
+      sendEmail(
+        user.email,
+        config.get('emailService.emailTemplates.forgotPassword'),
+        'Forgot password - HuyHung',
+        {
+          customerName: user.name,
+          customerEmail: user.email,
+          newPassword: password,
+          loginLink: config.get('loginLink'),
+        },
+      );
+
+      return {
+        success: true,
+        message: 'Password sent to your email',
+        result: { email: user.email, password: tempPassword },
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async findAll(type: string) {
+    try {
+      const users = await this.userDB.find({ type });
+      return {
+        succcess: true,
+        message: 'Users fetched successfully',
+        results: users,
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async updatePasswordOrName(
+    id: string,
+    { oldPassword, newPassword, name }: UpdateUserDto,
+  ) {
+    try {
+      if (!name && !newPassword) {
+        throw new BadRequestException('Please provide name or password');
+      }
+
+      const user = await this.userDB.findOne({
+        _id: id,
+      });
+      if (!user) {
+        throw new BadRequestException('User not found');
+      }
+
+      if (newPassword) {
+        const isPasswordMatch = await comparePassword(
+          oldPassword,
+          user.password,
+        );
+        if (!isPasswordMatch) {
+          throw new ForbiddenException('Invalid current password');
+        }
+        const password = await generateHashPassword(newPassword);
+        await this.userDB.updateOne(
+          {
+            _id: id,
+          },
+          {
+            password,
+          },
+        );
+      }
+
+      if (name) {
+        await this.userDB.updateOne(
+          {
+            _id: id,
+          },
+          {
+            name,
+          },
+        );
+      }
+      return {
+        success: true,
+        message: 'User updated successfully',
+        result: {
+          name: user.name,
+          email: user.email,
+          type: user.type,
+          id: user._id.toString(),
         },
       };
     } catch (error) {
