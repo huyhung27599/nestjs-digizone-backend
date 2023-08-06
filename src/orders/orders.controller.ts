7b804d0a90a618a757d814c4dac0ef9893624d76
huyhung27599 <huyhung27599@gmail.com>
2023-08-06 09:02:55 +0700

    Complete orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Headers,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { checkoutDtoArr } from './dto/checkout.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  async findAll(@Query('status') status: string, @Req() req: any) {
+    return await this.ordersService.findAll(status, req.user);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.ordersService.findOne(id);
+  }
+
+  @Post('/checkout')
+  async checkout(@Body() body: checkoutDtoArr, @Req() req: any) {
+    return await this.ordersService.checkout(body, req.user);
+  }
+
+  @Post('/webhook')
+  async webhook(
+    @Body() rawBody: Buffer,
+    @Headers('stripe-signature') sig: string,
+  ) {
+    return await this.ordersService.webhook(rawBody, sig);
+  }
+}
